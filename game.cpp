fd4943b30f437d3f1409d37d3c0fd90a6676ec97
justinNguyen <nguyenjustin@ufl.edu>
2024-11-29 17:43:40 -0500

    Implemented game class, outlined main.cpp

--- game.cpp
+++ game.cpp
@@ -2,3 +2,55 @@
 
 using namespace std;
 
+game::game(string name, int price, int releasedate, int reviews, int players) {
+    this->name = name;
+    this->price = price;
+    this->releasedate = releasedate;
+    this->reviews = reviews;
+    this->players = players;
+}
+string game::getname() {
+    return this->name;
+}
+
+int game::getprice() {
+    return this->price;
+}
+
+int game::getrelease() {
+    return this->releasedate;
+}
+
+int game::getreviews() {
+    return this->reviews;
+}
+
+int game::getplayers() {
+    return this->players;
+}
+
+void game::swap(game& swapee) {
+    string temp = this->name;
+    this->name = swapee.getname();
+    swapee.name = temp;
+
+    int temp2 = this->price;
+    this->price = swapee.getprice();
+    swapee.price = temp2;
+
+    temp2 = this->releasedate;
+    this->releasedate = swapee.getrelease();
+    swapee.releasedate = temp2;
+    
+    temp2 = this->reviews;
+    this->reviews = swapee.getreviews();
+    swapee.reviews = temp2;
+
+    temp2 = this->players;
+    this->players = swapee.getplayers();
+    swapee.players = temp2;
+}
+
+game::~game(){
+
+}
--- game.h
+++ game.h
@@ -1,4 +1,24 @@
 # pragma once
 
+#include <string>
 using namespace std;
+
+class game {
+private:
+    string name;
+    int price;
+    int releasedate;
+    int reviews;
+    int players;
+public:
+    game(string, int, int, int, int);
+    string getname();
+    int getprice();
+    int getrelease();
+    int getreviews();
+    int getplayers();
+    void swap(game&);
+    ~game();
+};
+
 
